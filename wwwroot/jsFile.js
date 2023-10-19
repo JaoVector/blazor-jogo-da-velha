858829e6eda4cc723822369e8f7dd6dd2a1fcb78
João Victor Carneiro Aureliano <jo4ovictor10@hotmail.com>
2023-10-19 15:32:34 -0300

    BlazorJogoDaVelha

---
+++ wwwroot/jsFile.js
@@ -0,0 +1,19 @@
+﻿window.ShowSwal = (player) => {
+    Swal.fire({
+        title: player + 'Ganhou',
+        width: 350,
+        padding: '3em',
+        color: '#716add',
+        backdrop: 'rgb(0,0,123,0.4) left top no-repeat'
+    })
+}
+
+window.ShowTie = () => {
+    Swal.fire({
+        title: 'Ninguém Ganhou',
+        width: 350,
+        padding: '3em',
+        color: '#716add',
+        backdrop: 'rgb(0,0,123,0.4) left top no-repeat'
+    })
+}
